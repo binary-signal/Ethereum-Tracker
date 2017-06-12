1d4e53784d5be09d80bb305b025b132da7f92cec
binary-signal <mvaggelis@gmail.com>
2017-06-13 01:22:43 +0300

    added percentage change alert via sms

--- config.py
+++ config.py
@@ -12,7 +12,14 @@ api_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 api_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 
+
 """ finance """
 investment = 50  # in euro
 fees = 2.96
 ticker = 'ETH/EUR'
+price_alert = 400
+percent_alert = 10 # 10%
+alert = True
+
+""" update interval for api calls """
+update_int = 300  # every 5 minutes
